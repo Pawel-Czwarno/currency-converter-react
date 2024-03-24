21587cd4b2b972a7e87f3106d7ea89f55b06fbe1
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-03-24 19:20:03 +0100

    Added Main and Header components

---
+++ src/Header/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Header = ({ title }) => (
+    <header class="header">
+        <h1 class="header__title"> { title } </h1>
+    </header>
+);
+
+export default Header;
