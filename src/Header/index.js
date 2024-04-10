6452b3f69f531f689b826c443197be088ad9ea13
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-04-10 19:35:57 +0200

    changed class to className

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
 const Header = ({ title }) => (
-    <header class="header">
-        <h1 class="header__title"> { title } </h1>
+    <header className="header">
+        <h1 className="header__title"> { title } </h1>
     </header>
 );
 
