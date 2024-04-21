cb1f6cb3fc26c64ce94ef7b92460835597d868cc
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-04-21 18:30:08 +0200

    Added div with children

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,13 +2,18 @@ import "./style.css";
 
 const Section = ({ children }) => (
     <section className="section">
-
-        <h2 className="section__title">Wybierz parę walutową, wpisz kwotę i przelicz wartość</h2>
-        <p className="section__legend">Kalkulator walutowy jest zgodny z kursem walut z dnia 10 kwietnia 2024 r.</p>
         <div>
-            {children}
+            {children[0]}
         </div>
-
+        <h2 className="section__title">
+            Wybierz parę walutową, wpisz kwotę i przelicz wartość
+        </h2>
+        <p className="section__legend">
+            Kalkulator walutowy jest zgodny z kursem walut z dnia 10 kwietnia 2024 r.
+        </p>
+        <div>
+            {children[1]}
+        </div>
     </section>
 );
 
