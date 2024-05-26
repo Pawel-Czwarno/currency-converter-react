23bbaff0d878c1e31f717a20c44f25eab61dc5f3
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-05-26 20:25:10 +0200

    Refactor DateComponent component to use styled-components

--- src/DateComponent/DateComponent.js
+++ src/DateComponent/DateComponent.js
@@ -1,18 +1,25 @@
 import useCurrentDate from "./useCurrentDate";
-import "./style.css";
+import styled from "styled-components";
+
+const Component = styled.p`
+    text-align: end;
+    padding-right: 20px;
+    font-size: 10px;
+    font-family: 'Noto Sans Mono', monospace;
+`;
 
 const DateComponent = () => {
     const currentTime = useCurrentDate();
-    
+
     return (
-        <p className="dateComponent">
+        <Component>
             Dzisiaj jest {currentTime.toLocaleString(
                 "pl", {
-                    weekday: "long", day: "numeric", month: "long",
+                weekday: "long", day: "numeric", month: "long",
                 hour: "numeric", minute: "numeric", second: "numeric"
             }
             )}
-        </p>
+        </Component>
     );
 };
 
